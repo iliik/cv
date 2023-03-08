c78bdc803960dba35d89b2171ee69729bc9f2365
Ilya <koselevicila@gmail.com>
2023-03-08 17:57:39 +0300

    Title-cv-refactor

--- src/options/Options.js
+++ src/options/Options.js
@@ -7,8 +7,8 @@ function Options() {
     return (
         <div className={style.optionsBlock}>
             <div className={`${styleContainer.container} ${style.optionsContainer}`}>
-                <h3 className={style.title}> Рассмотрю варинты удаленной работы</h3>
-                <button className={style.button}>Нанять меня</button>
+                <h3 className={style.h3}>Consider remote work options</h3>
+                <button className={style.button}>Hire me</button>
             </div>
         </div>
     );
