91aefcb9bd065a40e43eaa0cdd7738daf7437af5
Ilya <koselevicila@gmail.com>
2023-03-08 17:11:12 +0300

    Title-Projects

--- src/common/components/title/Title.js
+++ src/common/components/title/Title.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import style from './MyWorks.module.css'
-import styleContainer from "../common/styles/Container.module.css";
-import Work from "./work/Work";
+import style from './Title.module.css'
 
 
-function Title() {
+function Title(props) {
     return (
-        <div className={style.projectsBlock}>
-            <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-                <div className={style.title}>
-                    <h2>Projects</h2>
-                </div>
-                <div className={style.projects}>
-                    <Work title={"Social networc"} discription={'sed do eiusmod tempor incidiunt ut labox'}/>
-                    <Work title={"Todo list"} discription={'Lorem ipsum dolor sit amet, consectetur'}/>
-                </div>
-            </div>
+        <div className={style.title}>
+            <h2>{props.text}</h2>
         </div>
     );
 }
--- src/my-works/Projects.js
+++ src/my-works/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './Projects.module.css'
 import styleContainer from "../common/styles/Container.module.css";
 import Work from "./work/Work";
+import Title from "../common/components/title/Title";
 
 
 function Projects() {
@@ -9,7 +10,7 @@ function Projects() {
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <div className={style.title}>
-                    <h2>Projects</h2>
+                    <Title text={'Projects'}/>
                 </div>
                 <div className={style.projects}>
                     <Work title={"Social networc"} discription={'sed do eiusmod tempor incidiunt ut labox'}/>
