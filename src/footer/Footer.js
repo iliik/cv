b363445a37773980e9677c311a70628bc8b0c44d
Ilya <koselevicila@gmail.com>
2023-03-08 18:06:11 +0300

    Title-cv-refactor-Footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import style from './Footer.module.css'
 import styleContainer from "../common/styles/Container.module.css";
+import Title from "../common/components/title/Title";
 
 
 function Footer() {
     return (
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerBlock}`}>
-                <h2 className={style.name}>Илья Кошелевич</h2>
+           <Title text={'Ilya Koshelevich'}/>
                 <div className={style.iconBlock}>
                     <div className={style.icon}></div>
                     <div className={style.icon}></div>
