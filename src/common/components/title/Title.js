d17d607cf0486bf6d816f11c503d0ae866d37ccb
Ilya <koselevicila@gmail.com>
2023-03-14 15:21:56 +0300

    import-refactor

--- src/common/components/title/Title.js
+++ src/common/components/title/Title.js
@@ -5,7 +5,7 @@ import style from './Title.module.scss'
 function Title(props) {
     return (
         <div className={style.title}>
-            <h2>{props.text}</h2>
+            <h3>{props.text}</h3>
         </div>
     );
 }
