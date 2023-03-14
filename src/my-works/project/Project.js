ddb72f20c2136c9e3b3ffef37d47889b84770f35
Ilya <koselevicila@gmail.com>
2023-03-14 20:08:51 +0300

    import-refactor-Button-project

--- src/my-works/project/Project.js
+++ src/my-works/project/Project.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './Project.module.scss'
+import {Button} from "../../common/components/button/Button";
 
 function Project(props) {
     return (
         <div className={style.project}>
             <div className={style.image} style={props.style}>
-                <a className={style.video}>Watch</a>
+                <Button text={'Watch'}/>
             </div>
             <div className={style.projectInfo}>
                 <h3 className={style.projectTitle}>{props.title}</h3>
