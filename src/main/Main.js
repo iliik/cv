25947d946ac1c9de12cdb7396946f5bb1ec07cd8
Ilya <koselevicila@gmail.com>
2023-03-14 15:44:46 +0300

    import-Main-Photo

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,8 +1,14 @@
 import style from './Main.module.scss';
 import styleContainer from '../common/styles/Container.module.css'
+import Photo from '../assets/image/photo_2023-03-14_15-25-55.jpg'
 
 
 function Main() {
+
+    const imPhoto = {
+        backgroundImage: `url(${Photo})`,
+    }
+
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
@@ -11,7 +17,7 @@ function Main() {
                     <h1>I am Kashalevich Ilya</h1>
                     <p> Frontend Developer.</p>
                 </div>
-                <div className={style.photo}></div>
+                <div className={style.photo} style={imPhoto}></div>
             </div>
 
         </div>
