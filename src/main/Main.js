82073487e76a09272652f6f4c08809c0d416d2af
Ilya <koselevicila@gmail.com>
2023-03-15 13:16:40 +0300

    import-refactor-footer-main

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,14 +1,7 @@
 import style from './Main.module.scss';
-import styleContainer from '../common/styles/Container.module.css'
-import Photo from '../assets/image/photo_2023-03-14_15-25-55.jpg'
 
 
 function Main() {
-
-    const imPhoto = {
-        backgroundImage: `url(${Photo})`,
-    }
-
     return (
         <div className={style.mainBlock}>
             <div className={style.container}>
@@ -17,11 +10,10 @@ function Main() {
                     <span>I am  Ilya<span>Kashalevich</span></span>
                     <h1> Frontend Developer.</h1>
                 </div>
-                <div className={style.photo} >
+                <div className={style.photo}>
                     <div className={style.image}></div>
                 </div>
             </div>
-
         </div>
     );
 }
