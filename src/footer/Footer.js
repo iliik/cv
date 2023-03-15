39e8fa7f085faac738e5c000eccb6d3dbf05728e
Ilya <koselevicila@gmail.com>
2023-03-15 10:10:49 +0300

    import-refactor-footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -6,14 +6,14 @@ import Title from "../common/components/title/Title";
 
 function Footer() {
     return (
-        <div className={style.footerBlock}>
-            <div className={`${styleContainer.container} ${style.footerBlock}`}>
-                <Title text={'Ilya Koshelevich'}/>
-                <div className={style.iconBlock}>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
+        <div className={style.footer}>
+            <div className={style.container}>
+                <Title text={'Ilya Kashalevich'}/>
+                <div className={style.socialIcons}>
+                    <div className={style.socialIcon}><a href=''><img src={telegramIcon} alt=''/></a></div>
+                    <div className={style.socialIcon}><a href=''><img src={likedinIcon} alt=''/></a></div>
+                    <div className={style.socialIcon}><a href=''><img src={vkIcon} alt=''/></a></div>
+                    <div className={style.socialIcon}><a href=''><img src={instagramIcon} alt=''/></a></div>
                 </div>
                 <h2 className={style.date}> @2023 All rights reserved</h2>
             </div>
