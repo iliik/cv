25e10baa15a029f21e700ed3fa2918647be4f1f7
kosel <koselevicila@gmail.com>
2023-04-05 14:50:41 +0300

    Boous-efect

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,27 +1,31 @@
 import style from './Main.module.scss';
 import React from "react";
-import {Bounce} from "react-awesome-reveal";
+import {Bounce, Slide} from "react-awesome-reveal";
 import ReactTypingEffect from 'react-typing-effect';
-import { Tilt } from 'react-tilt'
+import {Tilt} from 'react-tilt'
 
 function Main() {
 
     return (
         <div id='main' className={style.mainBlock}>
             <div className={style.container}>
-                <Bounce>
+
+                <Slide>
                     <div className={style.greeting}>
                         <span>Hi There</span>
                         <span>I am  Ilya<span>Kashalevich</span></span>
                         <ReactTypingEffect text="Frontend Developer."/>
                     </div>
-                    <Tilt >
-                    <div className={style.photo}>
-                        <div className={style.image}></div>
-                    </div>
-                </Tilt>
+                </Slide>
 
+                <Bounce>
+                    <Tilt>
+                        <div className={style.photo}>
+                            <div className={style.image}></div>
+                        </div>
+                    </Tilt>
                 </Bounce>
+
             </div>
 
         </div>
--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -2,20 +2,23 @@ import React from 'react';
 import style from './Nav.module.scss';
 import 'animate.css';
 import {Link} from "react-scroll"
-
+import { Slide} from "react-awesome-reveal";
 
 function Nav() {
     return (
 
         <div className={style.nav}>
-            <Link className="animate__heartBeat" activeClass={style.active} to='main' spy={true} smooth={true}
-                  offset={0} duration={500}>Main</Link>
-            <Link className="animate__heartBeat" activeClass={style.active} to='skills' spy={true} smooth={true}
-                  offset={2} duration={500}>Skills</Link>
-            <Link className="animate__heartBeat" activeClass={style.active} to='projects' spy={true} smooth={true}
-                  offset={2} duration={500}>Project</Link>
-            <Link className="animate__heartBeat" activeClass={style.active} to='contact' spy={true} smooth={true}
-                  offset={-80} duration={500}>Contact</Link>
+            <Slide>
+                <Link  activeClass={style.active} to='main' spy={true} smooth={true}
+                      offset={0} duration={500}>Main</Link>
+                <Link  activeClass={style.active} to='skills' spy={true} smooth={true}
+                      offset={2} duration={500}>Skills</Link>
+                <Link  activeClass={style.active} to='projects' spy={true} smooth={true}
+                      offset={2} duration={500}>Project</Link>
+                <Link  activeClass={style.active} to='contact' spy={true} smooth={true}
+                      offset={-80} duration={500}>Contact</Link>
+            </Slide>
+
         </div>
     );
 }
