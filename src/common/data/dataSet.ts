9002124a88a53facbf73a2fc778efe1743395555
kosel <koselevicila@gmail.com>
2023-04-05 20:26:02 +0300

    refactor-Skill-map

--- src/common/data/dataSet.ts
+++ src/common/data/dataSet.ts
@@ -1,14 +1,14 @@
 import reduxIcon from "../../assets/iconProject/Redux.png";
-import reactIcon from "../../assets/iconProject/react-1.svg";
+import reactIcon from "../../assets/iconProject/[removal.ai]_tmp-642daebc5e36b.png";
 import storybookIcon from "../../assets/iconProject/storybook-icon.svg";
 import jsIcon from "../../assets/iconProject/Unofficial_JavaScript_logo_2.svg.png";
-import typescriptIcon from "../../assets/iconProject/[removal.ai]_tmp-642d9f24e2c72.png";
+import typeScriptIcon from "../../assets/iconProject/[removal.ai]_tmp-642d9f24e2c72.png";
 import sassIcon from "../../assets/iconProject/288_Sass-512.webp";
 import cssIcon from "../../assets/iconProject/CSS3_logo.svg.png";
 import reduxToolkitIcon from "../../assets/iconProject/redux-toolkit-1400.webp";
 import htmlIcon from "../../assets/iconProject/html-svgrepo-com.svg";
-import gitIcon from "../../assets/iconProject/png-clipart-github-repository-git-project-commit-github-angle-logo-thumbnail.png";
-import materiaUiIcon from "../../assets/iconProject/[removal.ai]_tmp-642daaa1032cb.png";
+import gitIcon from "../../assets/iconProject/git.png";
+import materialUiIcon from "../../assets/iconProject/[removal.ai]_tmp-642daaa1032cb.png";
 import restApiIcon  from "../../assets/iconProject/rest-api-1.svg";
 
 
@@ -17,14 +17,14 @@ const redux = {backgroundImage: `url(${reduxIcon})`}
 const react = {backgroundImage: `url(${reactIcon})`}
 const storybook = {backgroundImage: `url(${storybookIcon})`}
 const js = {backgroundImage: `url(${jsIcon})`}
-const typescript = {backgroundImage: `url(${typescriptIcon})`}
+const typeScript = {backgroundImage: `url(${typeScriptIcon})`}
 const sass = {backgroundImage: `url(${sassIcon})`,}
 const css = {backgroundImage: `url(${cssIcon})`}
 const reduxToolkit = {backgroundImage: `url(${reduxToolkitIcon})`}
 
 const html = {backgroundImage: `url(${htmlIcon})`}
 const git = {backgroundImage: `url(${gitIcon})`}
-const materiaUi = {backgroundImage: `url(${materiaUiIcon})`}
+const materialUi = {backgroundImage: `url(${materialUiIcon})`}
 const restApi = {backgroundImage: `url(${restApiIcon})`}
 
 
@@ -32,13 +32,13 @@ export const SkillsSet = [
     {style: redux, title: 'Redux'},
     {style: react, title: 'React'},
     {style: storybook, title: 'Storybook'},
-    {style: typescript, title: 'Typescript'},
+    {style: typeScript, title: 'TypeScript'},
     {style: sass, title: 'Sass'},
     {style: css, title: 'Css'},
     {style: js, title: 'JavaScript'},
     {style: html, title: 'Html'},
     {style: git, title: 'Git'},
-    {style: materiaUi, title: 'MateriaUi'},
+    {style: materialUi, title: 'MaterialUi'},
     {style: restApi, title: 'RestApi'},
     {style: reduxToolkit, title: 'ReduxToolkit'},
 ]
