16c71ff84886bd16697191b175b8b10fc7151761
kosel <koselevicila@gmail.com>
2023-04-05 16:21:15 +0300

    refactor-nav

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -13,9 +13,9 @@ function Nav() {
                 <Link  activeClass={style.active} to='skills' spy={true} smooth={true}
                       offset={-50} duration={500}>Skills</Link>
                 <Link  activeClass={style.active} to='projects' spy={true} smooth={true}
-                      offset={-45} duration={500}>Project</Link>
+                      offset={-45} duration={500}>Projects</Link>
                 <Link  activeClass={style.active} to='contact' spy={true} smooth={true}
-                      offset={-35} duration={500}>Contact</Link>
+                      offset={-35} duration={500}>Contacts</Link>
             </Slide>
 
         </div>
