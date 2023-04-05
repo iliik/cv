f9c79fba516c92823d7c36b462dc5956fbb36711
kosel <koselevicila@gmail.com>
2023-04-05 20:16:05 +0300

    refactor-Skill-map

--- src/common/data/dataSet.ts
+++ src/common/data/dataSet.ts
@@ -5,7 +5,12 @@ import jsIcon from "../../assets/iconProject/Unofficial_JavaScript_logo_2.svg.pn
 import typescriptIcon from "../../assets/iconProject/[removal.ai]_tmp-642d9f24e2c72.png";
 import sassIcon from "../../assets/iconProject/288_Sass-512.webp";
 import cssIcon from "../../assets/iconProject/CSS3_logo.svg.png";
-import reduxToolkitIcon from "../../assets/iconProject/mziq95fvwx3tt74icr2r.webp";
+import reduxToolkitIcon from "../../assets/iconProject/redux-toolkit-1400.webp";
+import htmlIcon from "../../assets/iconProject/html-svgrepo-com.svg";
+import gitIcon from "../../assets/iconProject/png-clipart-github-repository-git-project-commit-github-angle-logo-thumbnail.png";
+import materiaUiIcon from "../../assets/iconProject/[removal.ai]_tmp-642daaa1032cb.png";
+import restApiIcon  from "../../assets/iconProject/rest-api-1.svg";
+
 
 
 const redux = {backgroundImage: `url(${reduxIcon})`}
@@ -17,6 +22,11 @@ const sass = {backgroundImage: `url(${sassIcon})`,}
 const css = {backgroundImage: `url(${cssIcon})`}
 const reduxToolkit = {backgroundImage: `url(${reduxToolkitIcon})`}
 
+const html = {backgroundImage: `url(${htmlIcon})`}
+const git = {backgroundImage: `url(${gitIcon})`}
+const materiaUi = {backgroundImage: `url(${materiaUiIcon})`}
+const restApi = {backgroundImage: `url(${restApiIcon})`}
+
 
 export const SkillsSet = [
     {style: redux, title: 'Redux'},
@@ -25,6 +35,10 @@ export const SkillsSet = [
     {style: typescript, title: 'Typescript'},
     {style: sass, title: 'Sass'},
     {style: css, title: 'Css'},
-    {style: reduxToolkit, title: 'ReduxToolkit'},
     {style: js, title: 'JavaScript'},
+    {style: html, title: 'Html'},
+    {style: git, title: 'Git'},
+    {style: materiaUi, title: 'MateriaUi'},
+    {style: restApi, title: 'RestApi'},
+    {style: reduxToolkit, title: 'ReduxToolkit'},
 ]
