48b8e8ecfea76608cd25397d07e61d04caf78294
kosel <koselevicila@gmail.com>
2023-04-06 13:01:24 +0300

    refactor-data

--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import style from './Skill.module.scss'
 
-function Skill(props) {
-
+export function Skill(props) {
     return (
         <div className={style.skill}>
             <div style={props.style} className={style.icon}></div>
-
             <h3>{props.title}</h3>
-            {/*<span className={style.description}>{props.description}</span>*/}
         </div>
     );
 }
 
-export default Skill;
